8ea9ccd52eb3711c42c2a8407ff20932d40fb84e
Jaydeep Sarkar <jaydeepsarkr@gmail.com>
2024-12-11 14:05:46 +0530

    final modification done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Function to scroll to the target element
+document.addEventListener("DOMContentLoaded", () => {
+  const contactButton = document.getElementById("openContainer");
+  const containerDiv = document.querySelector(".container");
+  const closeButton = document.querySelector(".close-icon");
+
+  // Show the container div with animation
+  contactButton.addEventListener("click", () => {
+    containerDiv.classList.remove("hide");
+    containerDiv.classList.add("show");
+  });
+
+  // Hide the container div with animation
+  closeButton.addEventListener("click", () => {
+    containerDiv.classList.remove("show");
+    containerDiv.classList.add("hide");
+  });
+});
